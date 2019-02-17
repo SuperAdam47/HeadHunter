ad82019f9f5883d3575f32a030b323b86e24ca34
iNova <lynxuhc@gmail.com>
2019-02-17 07:56:44 -0400

    updated messages

--- src/icirgio/HeadHunter/Loader.php
+++ src/icirgio/HeadHunter/Loader.php
@@ -14,20 +14,17 @@ use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase implements Listener {
 
-    public function onLoad()
-    {
-        if($this->getServer()->getPluginManager()->getPlugin("EconomyAPI") != null) {
-            $this->getServer()->getLogger()->notice("EconomyAPI has been found");
-        } else {
-            $this->getServer()->getLogger()->notice("EconomyAPI HAS NOT been found, this will throw errors");
-        }
-    }
-
     public function onEnable()
     {
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
         $this->getServer()->getPluginManager()->registerEvents(new HeadHuntingEvent($this), $this);
+        $this->getServer()->getLogger()->info("Plugin HeadHunter Enabled!");
+    }
+
+    public function onDisable()
+    {
+        $this->getServer()->getLogger()->info("Plugin HeadHunter Disabled!");
     }
 }
